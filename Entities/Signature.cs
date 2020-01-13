8c3bde9950896dce870627aedb438ff1502b8027
Aviram Sharon <aviram.sharon@gmail.com>
2020-01-13 18:15:56 +0200

    From work

--- Entities/Signature.cs
+++ Entities/Signature.cs
@@ -1,10 +1,32 @@
 ﻿using MakeWell.Infrastructure;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MakeWell.Entities
 {
     public class Signature : ISignature
     {
+        public Signature()
+        {
+
+        }
+        
+        private static Signature SignSignature (string stamp = "")
+        {
+            Signature signature = new Signature();
+
+            var stampInfo = GetProperties(stamp);
+            if (true)
+            {
+                signature.ActionStamp = stampInfo["Action"];
+                signature.ContextStamp = stampInfo["Context"];
+                signature.UserStamp = stampInfo["User"];
+                signature.TimeStamp = DateTime.Now.ToString();
+            }
+            return signature;
+        }
+
         #region Key
         [Key]
         public int Id { get; set; }
@@ -25,6 +47,22 @@ namespace MakeWell.Entities
         #endregion
         
         public byte[] Timestamp { get; set; }
+
+        public static Signature Sign(string stamp = "User:MakeWell;Action:AddEntity;Context:Neveram;")
+        {
+            return SignSignature(stamp);
+        }
+
+        private static IDictionary<string, string> GetProperties(string stamp)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            foreach (string item in stamp.Split(";", StringSplitOptions.RemoveEmptyEntries))
+            {
+                string[] splittedItem = item.Split(":", StringSplitOptions.RemoveEmptyEntries);
+                result.Add(splittedItem[0], splittedItem[1]);
+            }
+            return result;
+        }
 
         public override string ToString()
         {
